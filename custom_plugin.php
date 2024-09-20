852e63af8762f3c4f8f16761e87ac476bf5450b3
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-20 18:25:36 +0600

    a

--- custom_plugin.php
+++ custom_plugin.php
@@ -21,6 +21,178 @@ function custom_plugin_add_html($content) {
         <div class="light">
         <div class="d-flex format-nav-link">
         <ul>
+            <li><a href="/">HEIC to JPG</a></li>
+            <li><a href="../heic-to-png" class="active">HEIC to PNG</a></li>
+            <li><a href="../heic-to-pdf">HEIC to PDF</a></li>
+        </ul>
+    </div>
+    <form id="convertForm" method="post" enctype="multipart/form-data">
+        <div class="d-block cursor-pointer hf-main-content"
+            style="pointer-events: none;">
+            <label class="d-block">
+                <input id="choose_image" maxuploads="2" accept=".heic" type="file" name="image" hidden
+                    onchange="loadFile(event)" multiple />
+                <div class="d-flex w-100 mt-2">
+                    <span id="convert-button" style="pointer-events: painted;"
+                        class="mt-2 d-flex btn bg-gradient text-white text-xl border rounded-md px-5 py-2 ml-auto mr-2">
+                        <i class="zp zp-upload mr-2"></i>Upload file</span>
+                    <a id="again-button" href="../heic-to-png" style="pointer-events: none; opacity: 0.5;"
+                        class="mt-2 d-flex btn bg-gradient text-white text-xl border rounded-md px-5 py-2 mr-auto ml-2">
+                        <i class="zp zp-clear mr-2"></i>Clear</a>
+                </div>
+            </label>
+            <div id="drop_down_body"
+                style="min-height: 170px; align-items: center; border-style: dashed; border-color: #b0b1b3;"
+                class="d-flex m-3 rounded justify-center cursor-pointer">
+
+                <span id="drag-drop-text" class="mx-auto"
+                    style="user-select: none;text-align: center;line-height: 2;font-weight: lighter;">Drag & Drop
+                    Files Here</span>
+
+
+                <div class=" w-100 drag-display grid-container" id="selected-image-grid">
+
+
+
+
+                </div>
+            </div>
+
+            <div class="d-flex my-4">
+                <span id="g_drive_btn" onclick="downloadAll()" class="mx-auto py-2 px-3 d-flex btn"><i
+                        class="mr-2 zp zp-download"></i>Download All <span class="download-count">0</span></span>
+
+            </div>
+            <span id="selected-file"></span>
+        </div>
+        <!-- <div class="d-flex mt-4 w-100">
+            <div class="w-50 mr-2">
+                <label class="mb-1">Quality</label>
+                <input id="quality_box" type="number" name="quality" min="0" max="100" value="70"
+                    class="w-100 bg-gray-100 rounded-md">
+            </div>
+            <div class="w-50 ml-2">
+                <label class="mb-1">Format</label>
+                <select name="format" id="select_format" class="w-100 bg-gray-100 rounded-md text-center">
+                    <option value="jpg">JPG</option>
+                    <option value="png">PNG</option>
+                    <option value="heic">HEIC</option>
+                </select>
+            </div>
+
+        </div> -->
+
+        <span id="server-message">
+            <!-- <div class="mt-4 hf-error">
+                    <div class="border px-3 py-5 text-center">
+                        <span class="font-medium">{ serverMessage }</span>
+                    </div>
+                </div> -->
+        </span>
+
+        <!-- <div class="d-flex mt-4 action-body">
+            <input type="submit" id="convert-button"
+                class="bg-primary btn bg-gradient text-white text-xl border rounded-md px-5 py-2 mx-auto"
+                value="Convert now" />
+
+
+
+
+        </div> -->
+    </form>
+        </div>
+        ';
+        $content = $custom_html;
+    }else if (is_page('heic-to-pdf')) {
+        $custom_html = '
+        <div class="light">
+        <div class="d-flex format-nav-link">
+        <ul>
+            <li><a href="/">HEIC to JPG</a></li>
+            <li><a href="../heic-to-png">HEIC to PNG</a></li>
+            <li><a href="../heic-to-pdf" class="active">HEIC to PDF</a></li>
+        </ul>
+    </div>
+    <form id="convertForm" method="post" enctype="multipart/form-data">
+        <div class="d-block cursor-pointer hf-main-content" style="pointer-events: none;">
+            <label class="d-block">
+                <input id="choose_image" maxuploads="2" accept=".heic" type="file" name="image" hidden
+                    onchange="loadFile(event)" multiple />
+                <div class="d-flex w-100 mt-2">
+                    <span id="convert-button" style="pointer-events: painted;"
+                        class="mt-2 d-flex btn bg-gradient text-white text-xl border rounded-md px-5 py-2 ml-auto mr-2">
+                        <i class="zp zp-upload mr-2"></i>Upload file</span>
+                    <a id="again-button" href="../heic-to-pdf" style="pointer-events: none; opacity: 0.5;"
+                        class="mt-2 d-flex btn bg-gradient text-white text-xl border rounded-md px-5 py-2 mr-auto ml-2">
+                        <i class="zp zp-clear mr-2"></i>Clear</a>
+                </div>
+            </label>
+            <div id="drop_down_body"
+                style="min-height: 170px; align-items: center; border-style: dashed; border-color: #b0b1b3;"
+                class="d-flex m-3 rounded justify-center cursor-pointer">
+
+                <span id="drag-drop-text" class="mx-auto"
+                    style="user-select: none;text-align: center;line-height: 2;font-weight: lighter;">Drag & Drop
+                    Files Here</span>
+
+
+                <div class=" w-100 drag-display grid-container" id="selected-image-grid">
+
+
+
+
+                </div>
+            </div>
+
+            <div class="d-flex my-4">
+                <span id="g_drive_btn" onclick="downloadAll()" class="mx-auto py-2 px-3 d-flex btn"><i
+                        class="mr-2 zp zp-download"></i>Download All <span class="download-count">0</span></span>
+
+            </div>
+            <span id="selected-file"></span>
+        </div>
+        <!-- <div class="d-flex mt-4 w-100">
+            <div class="w-50 mr-2">
+                <label class="mb-1">Quality</label>
+                <input id="quality_box" type="number" name="quality" min="0" max="100" value="70"
+                    class="w-100 bg-gray-100 rounded-md">
+            </div>
+            <div class="w-50 ml-2">
+                <label class="mb-1">Format</label>
+                <select name="format" id="select_format" class="w-100 bg-gray-100 rounded-md text-center">
+                    <option value="jpg">JPG</option>
+                    <option value="png">PNG</option>
+                    <option value="heic">HEIC</option>
+                </select>
+            </div>
+
+        </div> -->
+
+        <span id="server-message">
+            <!-- <div class="mt-4 hf-error">
+                    <div class="border px-3 py-5 text-center">
+                        <span class="font-medium">{ serverMessage }</span>
+                    </div>
+                </div> -->
+        </span>
+
+        <!-- <div class="d-flex mt-4 action-body">
+            <input type="submit" id="convert-button"
+                class="bg-primary btn bg-gradient text-white text-xl border rounded-md px-5 py-2 mx-auto"
+                value="Convert now" />
+
+
+
+
+        </div> -->
+    </form>
+        </div>
+        ';
+    }else{
+        $custom_html = '
+        <div class="light">
+        <div class="d-flex format-nav-link">
+        <ul>
             <li><a href="/" class="active">HEIC to JPG</a></li>
             <li><a href="../heic-to-png/">HEIC to PNG</a></li>
             <li><a href="../heic-to-pdf/">HEIC to PDF</a></li>
@@ -101,9 +273,6 @@ function custom_plugin_add_html($content) {
     </form>
         </div>
         ';
-        $content = $custom_html;
-    }else{
-        $custom_html = '<div>sssss</div>';
         $content = $custom_html;
     }
     return $content;
---
+++ js/png_script.js
@@ -0,0 +1,368 @@
+var totalImage = 0;
+var index = 0;
+var mainInde = 0;
+var beforeData = 0;
+const allConvertedFile = [];
+const itemsToRemove = [];
+const shouldStop = [];
+
+const dropArea = document.getElementById('drop_down_body');
+const fileInput = document.getElementById('choose_image');
+
+dropArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropArea.style.borderColor = 'blue';
+});
+
+dropArea.addEventListener('dragleave', () => {
+    dropArea.style.borderColor = '#ccc';
+});
+
+dropArea.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dropArea.style.borderColor = '#ccc';
+
+    const files = event.dataTransfer.files;
+    if (files.length > 0) {
+        fileInput.files = files;
+
+        // Create and dispatch a custom event
+        const changeEvent = new Event('change', { bubbles: true });
+        fileInput.dispatchEvent(changeEvent);
+    }
+});
+
+// Event listener for file input change event
+fileInput.addEventListener('change', loadFile);
+
+
+var loadFile = function (event) {
+    const input = event.target;
+    let maxFile = '<?php echo esc_html($setting_row->max_file_upload); ?>';
+    if (input.files.length > maxFile) {
+        alert("You can upload a maximum of " + maxFile + " files.");
+        return false;
+    }
+    document.getElementById("drop_down_body").style.pointerEvents = "none";
+   
+    document.getElementById("again-button").style.pointerEvents = "painted";
+    document.getElementById("again-button").style.opacity = "1";
+
+    if(totalImage == 0){
+        document.getElementById("drag-drop-text").remove();
+    }
+    
+    beforeData = totalImage;
+    totalImage = totalImage + input.files.length;
+    document.getElementsByClassName("download-count")[0].innerHTML = totalImage;
+    document.getElementById("selected-image-grid").classList.add("drag-display-mode");
+
+    for (mainInde; mainInde < totalImage; mainInde++) {
+        var selectedFileElement = document.getElementById('selected-image-grid');
+        var divRoot = document.createElement('div');
+
+        divRoot.id = "item-model-" + mainInde;
+        var gridDiv = document.createElement('div');
+        gridDiv.className = "grid-content";
+        var displayDiv = document.createElement('div');
+        displayDiv.className = "display-div";
+        var image = document.createElement('img');
+        image.className = "display-image";
+        var blurDiv = document.createElement('div');
+        blurDiv.className = "absolute blur-area";
+        var controlBox = document.createElement('div');
+        controlBox.className = "control-box";
+        var extraLayout = document.createElement('div');
+        extraLayout.className = "extra-box-layout";
+        var actionType = document.createElement('div');
+        actionType.className = "action-type";
+        var actionIcon = document.createElement('i');
+        actionIcon.className = "heic-action-icon zp zp-cloud-upload";
+        var actionText = document.createElement('span');
+        actionText.className = "heic-action-text";
+        actionText.innerHTML = "Uploading";
+        actionType.appendChild(actionIcon);
+        actionType.appendChild(actionText);
+        extraLayout.appendChild(actionType);
+        var progressHolder = document.createElement('div');
+        progressHolder.className = "progress-holder";
+        var progressParcent = document.createElement('span');
+        progressParcent.className = "progress-parcent";
+        progressParcent.innerHTML = "0%";
+        var uploadProgress = document.createElement('div');
+        uploadProgress.className = "upload-progress";
+        displayDiv.appendChild(image);
+        displayDiv.appendChild(blurDiv);
+        blurDiv.appendChild(controlBox);
+        controlBox.appendChild(extraLayout);
+        extraLayout.appendChild(progressHolder);
+        progressHolder.appendChild(progressParcent);
+        progressHolder.appendChild(uploadProgress);
+
+        gridDiv.appendChild(displayDiv);
+        divRoot.appendChild(gridDiv);
+
+        selectedFileElement.appendChild(divRoot);
+    }
+
+    converterLoop();
+
+    // if (input.files && input.files[0]) {
+    //     const file = input.files[0];
+    //     $('#selected-file').html('<div><span>.../'+file.name+'</span></div>');
+    //     var dropDownBody = document.getElementById("drop_down_body");
+    //     dropDownBody.style.opacity = "0.5";
+    //     dropDownBody.style.pointerEvents = "none";
+
+    //     var gDriveBtn = document.getElementById("g_drive_btn");
+    //     var dropBoxBtn = document.getElementById("drop_box_btn");
+    //     gDriveBtn.style.opacity = "0.5";
+    //     dropBoxBtn.style.opacity = "0.5";
+    //     gDriveBtn.style.cursor = "default";
+    //     dropBoxBtn.style.cursor = "default";
+
+
+    //     // // Check if the file is a HEIC file
+    //     // if (file.type === 'image/heic' || file.name.endsWith('.heic')) {
+    //     //     heic2any({
+    //     //         blob: file,
+    //     //         toType: "image/jpeg",
+    //     //     }).then(function (convertedBlob) {
+    //     //         const output = document.getElementById('output');
+    //     //         output.src = URL.createObjectURL(convertedBlob);
+    //     //         output.onload = function () {
+    //     //             URL.revokeObjectURL(output.src); // free memory
+    //     //         };
+    //     //     }).catch(function (error) {
+    //     //         console.error('Error converting HEIC to JPEG:', error);
+    //     //     });
+    //     // } else {
+    //     //     // Display image preview for non-HEIC images
+    //     //     const output = document.getElementById('output');
+    //     //     output.src = URL.createObjectURL(file);
+    //     //     output.onload = function () {
+    //     //         URL.revokeObjectURL(output.src); // free memory
+    //     //     };
+    //     //     console.log(file, output.src);
+    //     // }
+    // }
+
+};
+
+
+function converterLoop() {
+    for (index; index < totalImage; index++) {
+        uploadImage(index);
+    }
+}
+
+function uploadImage(layoutId) {
+    const fileInput = document.getElementById('choose_image');
+    var currentCount = layoutId - beforeData;
+    const file = fileInput.files[currentCount];
+
+    if (!file) {
+        alert('Please select a file.');
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('format', "png");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'wp-content/themes/heicfilter/ajax/convert.php', true);
+
+    xhr.upload.addEventListener('progress', function (event) {
+        if (event.lengthComputable) {
+            var percentComplete = (event.loaded / event.total) * 100;
+            document.getElementsByClassName("upload-progress")[layoutId].style.width = percentComplete + "%";
+            document.getElementsByClassName("progress-parcent")[layoutId].innerHTML = parseInt(percentComplete) + "%";
+            if (percentComplete >= 100) {
+                document.getElementsByClassName("upload-progress")[layoutId].style.width = "0%";
+                document.getElementsByClassName("progress-parcent")[layoutId].innerHTML = "0%";
+                document.getElementsByClassName("heic-action-icon")[layoutId].classList.remove("zp-cloud-upload");
+                document.getElementsByClassName("heic-action-icon")[layoutId].classList.add("zp-sync");
+                document.getElementsByClassName("heic-action-text")[layoutId].innerHTML = "Converting";
+                loopWithSleep(layoutId);
+            }
+        }
+    });
+
+    xhr.addEventListener('load', function () {
+        if (xhr.status === 200) {
+            shouldStop.push({ id: layoutId, status: true });
+            const response = JSON.parse(xhr.responseText);
+
+
+            var closeItem = document.createElement('i');
+            closeItem.style.pointerEvents = "painted";
+            closeItem.className = "zp zp-close";
+            closeItem.setAttribute("onclick", "itemClose('" + response.download_path + "', '" + layoutId + "')");
+            document.getElementsByClassName("blur-area")[layoutId].appendChild(closeItem);
+           
+            document.getElementsByClassName("extra-box-layout")[layoutId].style.display = "none";
+            const downloadLink = document.createElement('a');
+            downloadLink.setAttribute("target", "_blank");
+            downloadLink.setAttribute("href", "download?download="+response.download_link);
+            downloadLink.innerHTML = '<i class="zp zp-download"></i>Download';
+            document.getElementsByClassName("control-box")[layoutId].appendChild(downloadLink);
+            document.getElementsByClassName("control-box")[layoutId].classList.add("done");
+            
+            document.getElementsByClassName("display-image")[layoutId].src = "wp-content/uploads/heicfilter/" + response.preview_image;
+            allConvertedFile.push(response.download_path);
+            if (totalImage == index) {
+                document.getElementById("g_drive_btn").style.opacity = "1";
+                document.getElementById("g_drive_btn").style.pointerEvents = "painted";
+            } else {
+                document.getElementById("g_drive_btn").style.opacity = "0.5";
+                document.getElementById("g_drive_btn").style.pointerEvents = "none";
+            }
+
+            insertRecord(response.download_file, response.convert_type);
+        } else {
+            console.log('Upload failed!');
+        }
+    });
+
+    xhr.send(formData);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function loopWithSleep(layoutId) {
+    for (let i = 0; i < 100; i++) {
+        const item = shouldStop.find(obj => obj.id === layoutId);
+        const status = item ? item.status : null;
+        if (status) {
+            document.getElementsByClassName("upload-progress")[layoutId].style.width = "100%";
+            document.getElementsByClassName("progress-parcent")[layoutId].innerHTML = "100%";
+            break;
+        }
+        document.getElementsByClassName("upload-progress")[layoutId].style.width = i + "%";
+        document.getElementsByClassName("progress-parcent")[layoutId].innerHTML = i + "%";
+        await sleep(90); // Sleep for 1000 milliseconds (1 second)
+    }
+}
+
+function loadImage(url, layoutId, download_file, download_path, download_link) {
+    const xhr = new XMLHttpRequest();
+    // const progressBar = document.getElementById('progress-bar');
+    const img = document.getElementsByClassName('display-image')[layoutId];
+
+    xhr.open('GET', url, true);
+    xhr.responseType = 'blob';
+
+    xhr.onprogress = function (event) {
+        if (event.lengthComputable) {
+            const percentComplete = (event.loaded / event.total) * 100;
+            document.getElementsByClassName("upload-progress")[layoutId].style.width = percentComplete + "%";
+            document.getElementsByClassName("progress-parcent")[layoutId].innerHTML = parseInt(percentComplete) + "%";
+            console.log('Loading: ' + percentComplete + '%');
+        }
+    };
+
+    xhr.onload = function () {
+        if (this.status === 200) {
+            const blob = this.response;
+            const imgURL = URL.createObjectURL(blob);
+            img.src = imgURL;
+            img.style.display = 'block';
+            document.getElementsByClassName("control-box")[layoutId].innerHTML = '<a download="' + download_file + '" href="' + download_link + '"><i class="zp zp-download"></i>Download</a>';
+            document.getElementsByClassName("control-box")[layoutId].classList.add("done");
+
+            var closeItem = document.createElement('i');
+            closeItem.style.pointerEvents = "painted";
+            closeItem.className = "zp zp-close";
+            closeItem.setAttribute("onclick", "itemClose('" + download_path + "', '" + layoutId + "')");
+            document.getElementsByClassName("blur-area")[layoutId].appendChild(closeItem);
+        }
+    };
+
+    xhr.send();
+}
+
+function itemClose(data, id) {
+    itemsToRemove.push(data);
+    document.getElementById("item-model-" + id).remove();
+    let totalCount = allConvertedFile.length - itemsToRemove.length;
+    document.getElementsByClassName("download-count")[0].innerHTML = totalCount;
+    mainInde--;
+    index--;
+    totalImage--;
+    if (totalCount == 0) {
+        document.getElementById("g_drive_btn").style.opacity = "0.5";
+        document.getElementById("g_drive_btn").style.pointerEvents = "none";
+        window.location.href = "/";
+    }
+}
+
+function downloadFile(){
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'wp-content/themes/heicfilter/ajax/download_file.php', true);
+
+    xhr.onload = function () {
+
+    };
+
+    xhr.send(formData);
+}
+function downloadAll() {
+    for (let i = allConvertedFile.length - 1; i >= 0; i--) {
+        if (itemsToRemove.includes(allConvertedFile[i])) {
+            allConvertedFile.splice(i, 1);
+        }
+    }
+
+    document.getElementById("g_drive_btn").style.pointerEvents = "none";
+    document.getElementById("g_drive_btn").style.opacity = "0.5";
+
+    var data = {
+        files: allConvertedFile
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'wp-content/themes/heicfilter/ajax/download.php', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            document.getElementById("g_drive_btn").style.pointerEvents = "painted";
+            document.getElementById("g_drive_btn").style.opacity = "1";
+            const response = JSON.parse(xhr.responseText);
+            var link = document.createElement('a');
+            link.setAttribute('href', response.download_zip);
+            link.setAttribute('download', response.file_name);
+            link.click();
+        }
+    };
+    xhr.send(JSON.stringify(data));
+}
+document.getElementById("mobile-nav-btn").onclick = function (e) {
+
+    var nodeList = document.getElementById("nav-menu");
+    if (nodeList.style.left == "0px") {
+        nodeList.style.left = "-275px";
+        e.target.className = "zp zp-menu";
+    } else {
+        nodeList.style.left = "0px";
+        e.target.className = "zp zp-arrow-back";
+    }
+
+};
+
+function insertRecord(fileName, fileType) {
+    const formData = new FormData();
+    formData.append('file_name', fileName);
+    formData.append('file_type', fileType);
+
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', 'wp-content/themes/heicfilter/ajax/insert_record.php', true);
+
+    xhr.onload = function () {
+
+    };
+
+    xhr.send(formData);
+}
