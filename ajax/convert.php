11f6e9df7a95d944a93174238d92dc9f511b021f
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-25 18:16:31 +0600

    python

--- ajax/convert.php
+++ ajax/convert.php
@@ -17,33 +17,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $final_image = "heicconverter_" . $random_text . "." . $format;
         $cropFile = "cropped_" . $random_text . ".jpg";
         $croppedOutputPath = $path . $cropFile;
-        // Create an Imagick object
-        $imagick = new Imagick();
 
-        // Read the HEIC image
-        $imagick->readImage($source);
+        // // Create an Imagick object
+        // $imagick = new Imagick();
 
-        // Set the output format to JPG
-        $imagick->setImageFormat($format);
-        $imagick->setImageCompressionQuality(30);
+        // // Read the HEIC image
+        // $imagick->readImage($source);
 
-        // Write the image to the destination path
-        $imagick->writeImage($path . strtolower($final_image));
+        // // Set the output format to JPG
+        // $imagick->setImageFormat($format);
+        // $imagick->setImageCompressionQuality(30);
+
+        // // Write the image to the destination path
+        // $imagick->writeImage($path . strtolower($final_image));
 
 
 
 
-        $croppedImage = clone $imagick;
+        // $croppedImage = clone $imagick;
 
-        $croppedImage->resizeImage(200, 150, Imagick::FILTER_LANCZOS, 1); // Example resizing
+        // $croppedImage->resizeImage(200, 150, Imagick::FILTER_LANCZOS, 1); // Example resizing
 
-        $croppedImage->setImageCompressionQuality(30); // Set the quality (0-100)
+        // $croppedImage->setImageCompressionQuality(30); // Set the quality (0-100)
 
-        $croppedImage->writeImage($croppedOutputPath);
+        // $croppedImage->writeImage($croppedOutputPath);
 
-        // Clear resources
-        $imagick->clear();
-        $croppedImage->clear();
+        // // Clear resources
+        // $imagick->clear();
+        // $croppedImage->clear();
+
+        $command = escapeshellcmd("python3 ../script.py '$argument'");
+        // Execute the command
+        $output = shell_exec($command);
 
         // unlink($source);
 
@@ -53,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $response["preview_image"] = $cropFile;
         $response["preview_path"] = "../../wp-content/uploads/heicconverter/" . strtolower($final_image);
         $response["download_path"] = $path . strtolower($final_image);
-        $response["download_link"] = "https://heicjpgconverter.com/wp-content/uploads/heicconverter/" . strtolower($final_image);
+        $response["download_link"] = "https://heicjpgconverter.com/wp-content/uploads/heicconverter/" . strtolower($output);
         echo json_encode($response);
     }
 
