bb122d8892c2782a1f1700fc284c05eda93b3011
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-25 21:43:41 +0600

    python

--- ajax/convert.php
+++ ajax/convert.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // $imagick->clear();
         // $croppedImage->clear();
 
-        $command = escapeshellcmd("../script.sh 'turjo bd'");
+        $command = escapeshellcmd("../script.sh");
         // Execute the command
         $output = shell_exec($command);
 
