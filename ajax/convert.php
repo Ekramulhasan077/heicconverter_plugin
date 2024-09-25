14b9e468f9535e36e37b4f064070c7cc6f4c831e
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-26 05:46:46 +0600

    python

--- ajax/convert.php
+++ ajax/convert.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         // $imagick->clear();
         // $croppedImage->clear();
 
-        $command = escapeshellcmd("./../script.sh");
+        $command = escapeshellcmd("../script.sh");
         // Execute the command
         $output = shell_exec($command);
 
