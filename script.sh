972cf1720ca46b24a6d430dde37598bfd953e38a
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-26 16:53:20 +0600

    python

--- script.sh
+++ script.sh
@@ -3,6 +3,6 @@
 dirName=$1
 
 mkdir $(realpath "/var/www/heicconverter/wp-content/uploads/heicconverter/${dirName}")
-mkdir $(realpath "/var/www/heicconverter/wp-content/uploads/heicconverter/${dirName}")
+mkdir $(realpath "/var/www/heicconverter/wp-content/plugins/heicconverter_plugin/temp/${dirName}")
 chmod 777 $(realpath "/var/www/heicconverter/wp-content/uploads/heicconverter/${dirName}")
-chmod 777 $(realpath "/var/www/heicconverter/wp-content/uploads/heicconverter/${dirName}")
+chmod 777 $(realpath "/var/www/heicconverter/wp-content/plugins/heicconverter_plugin/temp/${dirName}")
