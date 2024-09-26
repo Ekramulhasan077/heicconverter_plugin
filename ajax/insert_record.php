9ed32b3f410c1d0bfba429ba6ea14a90e5e073ee
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-26 20:50:01 +0600

    python

--- ajax/insert_record.php
+++ ajax/insert_record.php
@@ -1,24 +1,34 @@
 <?php
-require_once('../../../../wp-load.php');
-/*
-Template Name: Insert Data Page
-*/
-get_header(); 
+// require_once('../../../../wp-load.php');
+// /*
+// Template Name: Insert Data Page
+// */
+// get_header(); 
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    global $wpdb;
+    // global $wpdb;
 
-    $table_name = $wpdb->prefix . 'heicfilter_media';
-    $data = array(
-        'file_name' => sanitize_text_field(strtolower(($_POST["file_name"]))),
-        'file_type' => sanitize_text_field($_POST["file_type"]),
-        'user_id' => sanitize_text_field($_COOKIE["user_id"]),
-    );
+    // $table_name = $wpdb->prefix . 'heicfilter_media';
+    // $data = array(
+    //     'file_name' => sanitize_text_field(strtolower(($_POST["file_name"]))),
+    //     'file_type' => sanitize_text_field($_POST["file_type"]),
+    //     'user_id' => sanitize_text_field($_COOKIE["user_id"]),
+    // );
 
-    $format = array('%s', '%s', '%s');
+    // $format = array('%s', '%s', '%s');
 
-    $wpdb->insert($table_name, $data, $format);
+    // $wpdb->insert($table_name, $data, $format);
 
    
+
+    if (isset($_COOKIE["convert_image"])) {
+
+        $countImg = $_COOKIE["convert_image"];
+        setcookie("dir_name", intval($countImg) + 1, time() + 31104000, "/", ".heicjpgconverter.com");
+        
+    }else{
+        $countImg = '0';
+        setcookie("dir_name", intval($countImg) + 1, time() + 31104000, "/", ".heicjpgconverter.com");
+    }
 }
 ?>
