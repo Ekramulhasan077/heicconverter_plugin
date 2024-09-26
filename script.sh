9ae44250d264c8efa6f11dd27bc9017e7baf5b11
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-26 16:46:43 +0600

    python

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@
 
 mkdir /var/www/heicconverter/wp-content/uploads/heicconverter/test
 mkdir /var/www/heicconverter/wp-content/plugins/heicconverter_plugin/temp/test
-chmod 666 /var/www/heicconverter/wp-content/uploads/heicconverter/test
-chmod 666 /var/www/heicconverter/wp-content/plugins/heicconverter_plugin/temp/test
+chmod 777 /var/www/heicconverter/wp-content/uploads/heicconverter/test
+chmod 777 /var/www/heicconverter/wp-content/plugins/heicconverter_plugin/temp/test
