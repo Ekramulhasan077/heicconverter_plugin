2c193ab33728716ae65a03919fa2a1e3a0e3c55e
Ekramulhasan077 <medhatv2023@gmail.com>
2024-09-29 10:18:43 +0600

    Bug fix

--- download.php
+++ download.php
@@ -6,6 +6,7 @@ $imageContent = file_get_contents($imageUrl);
 if ($imageContent === FALSE) {
     http_response_code(404);
     echo 'Image not found.';
+    echo '<script type="text/javascript">window.close();</script>';
     exit;
 }
 
@@ -21,7 +22,7 @@ header('Content-Length: ' . strlen($imageContent));
 
 echo $imageContent;
 echo '<script type="text/javascript">window.close();</script>';
-exit;
+// exit;
 
 
 
